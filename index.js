4b03f950ee14f90cdbef6c640bdd8512f263a3a9
Israel E. Filgueira <israel.eriston@gmail.com>
2018-04-25 19:44:53 -0500

    add eslint and reformat file

--- index.js
+++ index.js
@@ -1,59 +1,59 @@
-class Car {
-  constructor(count, currentState) {
-    this.count = 0
-    this.currentState = new Red(this)
-  }
-
-  change (state) {
-    if (this.count++ >= 10) {
-      return
+  class Car {
+    constructor(count, currentState) {
+      this.count = 0
+      this.currentState = new Red(this)
     }
-    this.currentState = state
-    this.currentState.go()
+
+    change (state) {
+      if (this.count++ >= 10) {
+        return
+      }
+      this.currentState = state
+      this.currentState.go()
+    }
+
+    start () {
+      this.currentState.go()
+    }
   }
 
-  start () {
-    this.currentState.go()
-  }
-}
-
-class Red  {
-  constructor(state) {
-  this.state = state
-}
-
-  go () {
-    console.log('Red -> for 1 minute')
-    this.state.change(new Green(this.state))
-  }
-
-}
-
-class Green  {
-  constructor(state) {
+  class Red  {
+    constructor(state) {
     this.state = state
   }
 
     go () {
-        console.log('Green -> for 1 minute')
-        this.state.change(new Yellow(this.state))
+      console.log('Red -> for 1 minute')
+      this.state.change(new Green(this.state))
     }
-}
 
-class Yellow  {
-  constructor(state) {
-    this.state = state
   }
 
-    go () {
-        console.log('Yellow -> for 10 minute')
-        this.state.change(new Red(this.state))
+  class Green  {
+    constructor(state) {
+      this.state = state
     }
-}
 
-const run = () => {
-    const car = new Car()
-    car.start()
+      go () {
+          console.log('Green -> for 1 minute')
+          this.state.change(new Yellow(this.state))
+      }
+  }
 
-}
-run()
+  class Yellow  {
+    constructor(state) {
+      this.state = state
+    }
+
+      go () {
+          console.log('Yellow -> for 10 minute')
+          this.state.change(new Red(this.state))
+      }
+  }
+
+  const run = () => {
+      const car = new Car()
+      car.start()
+
+  }
+  run()
