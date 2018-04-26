50a07812bb56674cdff68dc11604c757a4ad1aae
Israel E. Filgueira <israel.eriston@gmail.com>
2018-04-25 22:48:36 -0500

    adjustment eslint

--- index.js
+++ index.js
@@ -1,59 +1,57 @@
-  class Car {
-    constructor(count, currentState) {
-      this.count = 0
-      this.currentState = new Red(this)
-    }
-
-    change (state) {
-      if (this.count++ >= 10) {
-        return
-      }
-      this.currentState = state
-      this.currentState.go()
-    }
-
-    start () {
-      this.currentState.go()
-    }
+class Car {
+  constructor (count, currentState) {
+    this.count = 0
+    this.currentState = new Red(this)
   }
 
-  class Red  {
-    constructor(state) {
+  change (state) {
+    if (this.count++ >= 10) {
+      return
+    }
+    this.currentState = state
+    this.currentState.go()
+  }
+
+  start () {
+    this.currentState.go()
+  }
+}
+
+class Red {
+  constructor (state) {
     this.state = state
   }
 
-    go () {
-      console.log('Red -> for 1 minute')
-      this.state.change(new Green(this.state))
-    }
+  go () {
+    console.log('Red -> for 1 minute')
+    this.state.change(new Green(this.state))
+  }
+}
 
+class Green {
+  constructor (state) {
+    this.state = state
   }
 
-  class Green  {
-    constructor(state) {
-      this.state = state
-    }
+  go () {
+    console.log('Green -> for 1 minute')
+    this.state.change(new Yellow(this.state))
+  }
+}
 
-      go () {
-          console.log('Green -> for 1 minute')
-          this.state.change(new Yellow(this.state))
-      }
+class Yellow {
+  constructor (state) {
+    this.state = state
   }
 
-  class Yellow  {
-    constructor(state) {
-      this.state = state
-    }
-
-      go () {
-          console.log('Yellow -> for 10 minute')
-          this.state.change(new Red(this.state))
-      }
+  go () {
+    console.log('Yellow -> for 10 minute')
+    this.state.change(new Red(this.state))
   }
+}
 
-  const run = () => {
-      const car = new Car()
-      car.start()
-
-  }
-  run()
+const run = () => {
+  const car = new Car()
+  car.start()
+}
+run()
